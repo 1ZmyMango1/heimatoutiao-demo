12bf1eac25dc7d4e64e5c434873cd83f6dc460a5
zhangmengyang <2655419131@qq.com>
2022-07-03 14:56:52 +0800

    封装了icon组件

---
+++ src/components/ToutiaoIcon.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <!-- 封装 -->
+    <i :class="['toutiao', `toutiao-${icon}`]"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  // 父传子
+  props: {
+    icon: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style></style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,14 @@ import App from './App.vue'
 import router from './routers'
 import store from './store'
 
+// 引入样式
 import './styles/index.less'
+
+// 引入组件
+import ToutiaoIcon from './components/ToutiaoIcon'
+
+// 注册全局组件
+Vue.component('ToutiaoIcon', ToutiaoIcon)
 
 Vue.config.productionTip = false
 
