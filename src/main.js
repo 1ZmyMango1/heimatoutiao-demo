8187c03dd7232321b8fd2bdf174aed3a16a72b27
zhangmengyang <2655419131@qq.com>
2022-07-03 11:59:06 +0800

    修改了目录结构

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './routers'
 import store from './store'
+
+import './styles/index.less'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
